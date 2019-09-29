0d229ba1a120c359fbd765f9e097e1c5c1dc91fa
Kim gichan <geatrigger@gmail.com>
2019-09-29 16:19:30 +0900

    Make basic stage

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float sensitivity = 700.0f;
+
+    float rotationX = 0;
+    float rotationY = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouserMoveValueX = Input.GetAxis("Mouse X");
+        float mouserMoveValueY = Input.GetAxis("Mouse Y");
+
+        rotationX += mouserMoveValueX * sensitivity * Time.deltaTime;
+        rotationY += mouserMoveValueY * sensitivity * Time.deltaTime;
+
+        rotationY = Mathf.Clamp(rotationY, -50.0f, 60.0f);
+
+        transform.eulerAngles = new Vector3(-rotationY, rotationX, 0.0f);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public Transform cameraTransform;
+    CharacterController characterController = null;
+
+    //중력
+    float yVelocity = 0.0f;
+    public float gravity = -20.0f;
+    //점프
+    public float jumpSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(x, 0, z);
+        moveDirection = cameraTransform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+        characterController.Move(moveDirection * Time.deltaTime);
+       
+        if (Input.GetButtonDown("Jump"))
+        {
+            yVelocity = jumpSpeed;
+        }
+        yVelocity += gravity * Time.deltaTime;
+        moveDirection.y = yVelocity;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if(characterController.collisionFlags == CollisionFlags.Below)
+        {
+            yVelocity = 0.0f;
+        }
+    }
+}
