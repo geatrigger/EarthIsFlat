8bcf9f3b9341d3bf7708cfa2e848348c3f036f80
natonato <topgun0209@naver.com>
2019-09-29 19:36:33 +0900

    test code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
+    public float moveSpeed = 25.0f;
+    public float runSpeed = 40.0f;
+    float curSpeed;
+    float accel = 10.0f;
+    float decel = 10.0f;
     public Transform cameraTransform;
+
     CharacterController characterController = null;
 
     //중력
@@ -18,17 +23,60 @@ public class PlayerController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
     }
-
+    Vector3 moveDirection =new Vector3(0, 0, 0);
+    Vector3 tempDirection = new Vector3(0, 0, 0);
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(x, 0, z);
+        // running
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (moveDirection.x != x || moveDirection.z != z)
+            {
+
+            }
+            if (curSpeed < runSpeed)
+            {
+
+                curSpeed += accel * Time.deltaTime;
+            }
+            if (moveDirection.x == x && moveDirection.z == z)
+            {
+                tempDirection = moveDirection;
+            }
+        }
+        //walking
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            moveDirection = new Vector3(x, 0, z);
+            if (curSpeed < moveSpeed)
+            {
+
+                curSpeed += accel * Time.deltaTime;
+            }
+            else//running -> walking
+            {
+                curSpeed -= decel * Time.deltaTime;
+            }
+            if (moveDirection.x == x && moveDirection.z == z)
+            {
+                tempDirection = moveDirection;
+            }
+        }
+        else
+        {
+            moveDirection = tempDirection;
+            if (curSpeed > 0)
+            {
+                curSpeed -= decel * Time.deltaTime;
+            }
+            else curSpeed = 0;
+        }
         moveDirection = cameraTransform.TransformDirection(moveDirection);
-        moveDirection *= moveSpeed;
-        characterController.Move(moveDirection * Time.deltaTime);
-       
+        moveDirection *= curSpeed;
+
         if (Input.GetButtonDown("Jump"))
         {
             yVelocity = jumpSpeed;
