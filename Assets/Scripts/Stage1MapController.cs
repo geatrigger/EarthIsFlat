20f37ebb00871c6bad9ef6649116d1fba54a3b04
Kim gichan <geatrigger@gmail.com>
2019-10-03 17:24:44 +0900

    Add timezone system

--- Assets/Scripts/Stage1MapController.cs
+++ Assets/Scripts/Stage1MapController.cs
@@ -2,42 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Floor;
+
+
 public class Stage1MapController : MonoBehaviour
 {
     public List<GameObject> movingPlatforms;
     public List<List<FloorOrder>> ordersList;
     float time;
+    int timeZone;
     // Start is called before the first frame update
     void Start()
     {
         time = 0;
-        ordersList = new List<List<FloorOrder>>();
-
-        List<FloorOrder> po1 = new List<FloorOrder>();
-        po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po1.Add(new FloorOrder(FloorStates.Backward, 4.0f));
-        po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po1.Add(new FloorOrder(FloorStates.Forward, 4.0f));
-        ordersList.Add(po1);
-
-
-        List<FloorOrder> po2 = new List<FloorOrder>();
-        po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po2.Add(new FloorOrder(FloorStates.Left, 4.0f));
-        po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po2.Add(new FloorOrder(FloorStates.Right, 4.0f));
-        ordersList.Add(po2);
-
-        List<FloorOrder> po3 = new List<FloorOrder>();
-        po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po3.Add(new FloorOrder(FloorStates.Up, 4.0f));
-        po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
-        po3.Add(new FloorOrder(FloorStates.Down, 4.0f));
-        ordersList.Add(po3);
-
-        OrderSystemOnline();
+        timeZone = 0;
+        StartTimeZone(1);
     }
 
+    public void StartTimeZone(int state)
+    {
+        if (state == 0)
+        {
+            ordersList = new List<List<FloorOrder>>();
+
+            List<FloorOrder> po1 = new List<FloorOrder>();
+            po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po1.Add(new FloorOrder(FloorStates.Backward, 4.0f));
+            po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po1.Add(new FloorOrder(FloorStates.Forward, 4.0f));
+            ordersList.Add(po1);
+
+            List<FloorOrder> po2 = new List<FloorOrder>();
+            po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po2.Add(new FloorOrder(FloorStates.Left, 4.0f));
+            po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po2.Add(new FloorOrder(FloorStates.Right, 4.0f));
+            ordersList.Add(po2);
+
+            List<FloorOrder> po3 = new List<FloorOrder>();
+            po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po3.Add(new FloorOrder(FloorStates.Up, 4.0f));
+            po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po3.Add(new FloorOrder(FloorStates.Down, 4.0f));
+            ordersList.Add(po3);
+        }
+        else if(state == 1)
+        {
+            ordersList = new List<List<FloorOrder>>();
+
+            List<FloorOrder> po1 = new List<FloorOrder>();
+            po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po1.Add(new FloorOrder(FloorStates.Backward, 8.0f));
+            po1.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po1.Add(new FloorOrder(FloorStates.Forward, 8.0f));
+            ordersList.Add(po1);
+
+            List<FloorOrder> po2 = new List<FloorOrder>();
+            po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po2.Add(new FloorOrder(FloorStates.Left, 8.0f));
+            po2.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po2.Add(new FloorOrder(FloorStates.Right, 8.0f));
+            ordersList.Add(po2);
+
+            List<FloorOrder> po3 = new List<FloorOrder>();
+            po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po3.Add(new FloorOrder(FloorStates.Down, 4.0f));
+            po3.Add(new FloorOrder(FloorStates.Idle, 3.0f));
+            po3.Add(new FloorOrder(FloorStates.Up, 4.0f));
+            ordersList.Add(po3);
+
+        }
+        OrderSystemOnline();
+    }
+    public void EndTimeZone(int state)
+    {
+        //
+    }
+
+    public void ChangeTimeZone(int currentState, int nextState)
+    {
+        if(currentState == 0 && nextState == 1)
+        {
+
+        }
+        else if(currentState == 1 && nextState == 0)
+        {
+
+        }
+        StartTimeZone(nextState);
+    }
     // Update is called once per frame
     void Update()
     {
