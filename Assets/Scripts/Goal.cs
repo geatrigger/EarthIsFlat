86b22154c1c0286e5239f21ab26cdd2c626ae321
redsuncore <yjh4645@korea.ac.kr>
2019-10-04 17:59:35 +0900

    GOAL Added
    Goal System Uploaded

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Image panel;
+    public Text GameOverText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.enabled = false;
+        GameOverText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            CharacterController controller = other.GetComponent<CharacterController>();
+            controller.enabled = false;
+            GameOverText.text = "=!=TEST_SUCCESS=!=";
+            panel.enabled = true;
+            GameOverText.enabled = true;
+        }
+    }
+}
