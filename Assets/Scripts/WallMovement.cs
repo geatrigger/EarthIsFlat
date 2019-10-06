3958b772b1eb32b77a3c13daf3bdc3687317f653
nato <topgun0209@naver.com>
2019-10-06 20:11:47 +0900

    OrderToWall just one line (wall disappear)

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    public GameObject me;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OrderToWall(bool state)
+    {
+        me.SetActive(state);
+        //달리 할게 없음
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
