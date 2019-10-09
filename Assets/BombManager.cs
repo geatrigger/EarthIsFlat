ede6c89bca431983b64cab5e18c6d4c31176bac6
nato <topgun0234@gmail.com>
2019-10-09 20:14:44 +0900

    canon fire ball

---
+++ Assets/BombManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombManager : MonoBehaviour
+{
+    float time;
+    bool PlayerIsHit;
+    GameObject Player;
+    Vector3 hitdir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerIsHit = false;
+        time=0.0f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Player = collision.gameObject;
+            //do something
+            PlayerIsHit = true;
+            hitdir = collision.gameObject.transform.position - transform.position;
+            
+
+            Debug.Log(hitdir);
+            //collision.gameObject.GetComponent<Rigidbody>().AddForce(dir * 3000.0f);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerIsHit == true)
+        {
+
+            StartCoroutine("pushPlayer");
+        }
+        time += Time.deltaTime;
+        if (time > 6.0 && PlayerIsHit == false)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator pushPlayer()
+    {
+        time = 0.0f;
+        while (time < 0.5)
+        {
+            hitdir.y = 2.0f * Mathf.Lerp(1.0f, -0.5f, time*2);
+            time += Time.deltaTime;
+            Player.transform.Translate(hitdir * 1.0f * Time.deltaTime);
+
+            yield return null;
+
+        }
+        PlayerIsHit = false;
+
+
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/FireManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    public GameObject Canon;
+    public GameObject CanonBall;
+
+    Vector3 FireVector;
+    Vector3 FirePosition;
+    float timer =0.0f;
+    float power = 20.0f;
+    bool fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire = true;//or flase
+        FireVector = Canon.transform.GetChild(0).transform.forward;
+        FirePosition = Canon.transform.GetChild(0).transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3.0f && fire)
+        {
+            GameObject obj = Instantiate(CanonBall);
+
+            obj.transform.position = FirePosition;
+            obj.GetComponent<Rigidbody>().velocity = FireVector * power;
+            timer = 0.0f;
+        }
+    }
+
+    public void OrderToCanon(bool state)
+    {
+        Canon.transform.GetChild(1).gameObject.SetActive(state);
+        fire = state;
+
+    }
+}
